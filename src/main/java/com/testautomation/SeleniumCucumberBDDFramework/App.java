ef9fd97875ec7139a6571f280f259dad80be708e
kmellero <ktm000@yahoo.com>
2020-06-08 21:20:13 +0200

    overdue first commit/push from local

---
+++ src/main/java/com/testautomation/SeleniumCucumberBDDFramework/App.java
@@ -0,0 +1,13 @@
+package com.testautomation.SeleniumCucumberBDDFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
